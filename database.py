a4297f01ce91bd707a16f13ca11c87e6de920a7a
shurubaru <sebastianhurubaru@gmail.com>
2019-05-25 18:28:58 +0200

    Revert "Revert "data_and_correction""
    This reverts commit 4dc6dec0703227e2df75e161915899bfc20a7487.

---
+++ database.py
@@ -0,0 +1,35 @@
+import cx_Oracle
+
+connection = cx_Oracle.connect("CS221", "CS221", "localhost/SEBI.168.86.68")
+
+cursor = connection.cursor()
+
+
+def insertCompany(company_number, profile, officers, persons_with_significant_control):
+
+    # statement = 'insert into company_profile(company_number, profile, officers, persons_with_significant_control) values (:2, :3, :4, :5)'
+    statement = 'MERGE INTO company USING dual ON ( company_number=:2 ) \
+                WHEN MATCHED THEN UPDATE SET profile=:3 , officers=:4, persons_with_significant_control=:5 \
+                WHEN NOT MATCHED THEN INSERT (company_number, profile, officers, persons_with_significant_control)  \
+                VALUES (:2, :3, :4, :5)'
+    cursor.execute(statement, (company_number, profile, officers, persons_with_significant_control))
+    connection.commit()
+
+
+def insertOfficer(officer_id, appointments):
+    # statement = 'insert into officer(officer_id, appointments) values (:2, :3)'
+    statement = 'MERGE INTO officer USING dual ON ( officer_id = :2 ) \
+                    WHEN MATCHED THEN UPDATE SET appointments =:3  \
+                    WHEN NOT MATCHED THEN INSERT (officer_id, appointments)  \
+                    VALUES (:2, :3)'
+    cursor.execute(statement, (officer_id, appointments))
+    connection.commit()
+
+def updateCompanyPSC(company_number, persons_with_significant_control):
+    statement = 'UPDATE company SET persons_with_significant_control = :v WHERE company_number = :k'
+    cursor.execute(statement, {'k': company_number, 'v': persons_with_significant_control})
+    connection.commit()
+
+
+
+
--- search.py
+++ search.py
@@ -15,7 +15,7 @@ from jsonpath_ng import jsonpath, parse
 
 API_KEY="0K5Fn3TguQ_1OdFoSuzQREVG7aee1OKSYS5Mj5ns"
 MAX_REQ_FOR_KEY=600
-TIMEOUT=600 #seconds
+TIMEOUT=300 #seconds
 BASE_URL="https://api.companieshouse.gov.uk/search"
 
 
